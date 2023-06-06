5b48f19514983d7f7a6c9048f56ef539a64204f3
arbyun <someonealiveh@gmail.com>
2023-06-06 12:51:04 +0100

    refactor: cards variables names

--- TragicTheReckoningGame/Deck.cs
+++ TragicTheReckoningGame/Deck.cs
@@ -24,32 +24,32 @@ namespace TragicTheReckoningGame
             Card newCard = new Card("Flying Wand", 1, 1, 1, this);
             Populate(CardList, newCard, 4);
 
-            Card newCard = new Card("Severed Monkey Head", 1, 2, 1, this);
-            Populate(CardList, newCard, 4);
+            Card newCard1 = new Card("Severed Monkey Head", 1, 2, 1, this);
+            Populate(CardList, newCard1, 4);
 
-            Card newCard = new Card("Mystical Rock Wall", 2, 0, 5, this);
-            Populate(CardList, newCard, 2); 
+            Card newCard2 = new Card("Mystical Rock Wall", 2, 0, 5, this);
+            Populate(CardList, newCard2, 2); 
 
-            Card newCard = new Card("Lobster McCrabs", 2, 1, 3, this);
-            Populate(CardList, newCard, 2);
+            Card newCard3 = new Card("Lobster McCrabs", 2, 1, 3, this);
+            Populate(CardList, newCard3, 2);
 
-            Card newCard = new Card("Goblin Troll", 3, 3, 2, this);
-            Populate(CardList, newCard, 2);
+            Card newCard4 = new Card("Goblin Troll", 3, 3, 2, this);
+            Populate(CardList, newCard4, 2);
 
-            Card newCard = new Card("Scortching Heatwave", 4, 5, 3, this);
-            Populate(CardList, newCard, 1);
+            Card newCard5 = new Card("Scorching Heatwave", 4, 5, 3, this);
+            Populate(CardList, newCard5, 1);
 
-            Card newCard = new Card("Blind Minotaur", 3, 1, 3, this);
-            Populate(CardList, newCard, 1);
+            Card newCard6 = new Card("Blind Minotaur", 3, 1, 3, this);
+            Populate(CardList, newCard6, 1);
 
-            Card newCard = new Card("Tim, The Wizard", 5, 6, 4, this);
-            Populate(CardList, newCard, 1);
+            Card newCard7 = new Card("Tim, The Wizard", 5, 6, 4, this);
+            Populate(CardList, newCard7, 1);
 
-            Card newCard = new Card("Sharply Dressed", 4, 3, 3, this);
-            Populate(CardList, newCard, 1);
+            Card newCard8 = new Card("Sharply Dressed", 4, 3, 3, this);
+            Populate(CardList, newCard8, 1);
 
-            Card newCard = new Card("Blue Steel", 2, 2, 2, this);
-            Populate(CardList, newCard, 2);
+            Card newCard9 = new Card("Blue Steel", 2, 2, 2, this);
+            Populate(CardList, newCard9, 2);
             
         }
 
